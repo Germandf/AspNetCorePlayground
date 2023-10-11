4100b0e55cbdc42ab9b3e9bfeb4e01304984adfb
Germán De Francesco <germandefrancesco@hotmail.com>
2023-10-10 22:33:41 -0300

    Clean up SignUpHandlerTests

--- src/Api.UnitTests/Features/SignUp/SignUpHandlerTests.cs
+++ src/Api.UnitTests/Features/SignUp/SignUpHandlerTests.cs
@@ -12,6 +12,8 @@ public class SignUpHandlerTests
     private readonly IEmailIsUniqueRepository _emailIsUniqueRepository;
     private readonly ICreateUserRepository _createUserRepository;
     private readonly SignUpHandler _sut;
+    private readonly SignUpRequest _request;
+    private readonly User _user;
 
     public SignUpHandlerTests()
     {
@@ -19,17 +21,27 @@ public class SignUpHandlerTests
         _emailIsUniqueRepository = Substitute.For<IEmailIsUniqueRepository>();
         _createUserRepository = Substitute.For<ICreateUserRepository>();
         _sut = new SignUpHandler(_userNameIsUniqueRepository, _emailIsUniqueRepository, _createUserRepository);
+        _request = _fixture.Create<SignUpRequest>();
+        _userNameIsUniqueRepository
+            .UserNameIsUniqueAsync(_request.UserName, default)
+            .Returns(true);
+        _emailIsUniqueRepository
+            .EmailIsUniqueAsync(_request.Email, default)
+            .Returns(true);
+        _user = _fixture.Create<User>();
+        _createUserRepository
+            .CreateUserAsync(_request.UserName, _request.Email, _request.Password, default)
+            .Returns(_user);
     }
 
     [Fact]
     public async Task UserNameIsTaken_ReturnsError()
     {
-        var request = _fixture.Create<SignUpRequest>();
         _userNameIsUniqueRepository
-            .UserNameIsUniqueAsync(request.UserName, default)
+            .UserNameIsUniqueAsync(_request.UserName, default)
             .Returns(false);
 
-        var response = await _sut.Handle(request, default);
+        var response = await _sut.Handle(_request, default);
 
         response.IsFailed.Should().BeTrue();
         response.Errors.Should().Contain(x => x.Message == ErrorMessages.UserNameIsTaken);
@@ -38,15 +50,11 @@ public class SignUpHandlerTests
     [Fact]
     public async Task EmailIsTaken_ReturnsError()
     {
-        var request = _fixture.Create<SignUpRequest>();
-        _userNameIsUniqueRepository
-            .UserNameIsUniqueAsync(request.UserName, default)
-            .Returns(true);
         _emailIsUniqueRepository
-            .EmailIsUniqueAsync(request.Email, default)
+            .EmailIsUniqueAsync(_request.Email, default)
             .Returns(false);
 
-        var response = await _sut.Handle(request, default);
+        var response = await _sut.Handle(_request, default);
 
         response.IsFailed.Should().BeTrue();
         response.Errors.Should().Contain(x => x.Message == ErrorMessages.EmailIsTaken);
@@ -55,23 +63,11 @@ public class SignUpHandlerTests
     [Fact]
     public async Task RequestIsValid_ReturnsUserResponse()
     {
-        var request = _fixture.Create<SignUpRequest>();
-        _userNameIsUniqueRepository
-            .UserNameIsUniqueAsync(request.UserName, default)
-            .Returns(true);
-        _emailIsUniqueRepository
-            .EmailIsUniqueAsync(request.Email, default)
-            .Returns(true);
-        var user = _fixture.Create<User>();
-        _createUserRepository
-            .CreateUserAsync(request.UserName, request.Email, request.Password, default)
-            .Returns(user);
-
-        var response = await _sut.Handle(request, default);
+        var response = await _sut.Handle(_request, default);
 
         response.IsSuccess.Should().BeTrue();
-        response.Value.Id.Should().Be(user.Id);
-        response.Value.UserName.Should().Be(user.UserName);
-        response.Value.Email.Should().Be(user.Email);
+        response.Value.Id.Should().Be(_user.Id);
+        response.Value.UserName.Should().Be(_user.UserName);
+        response.Value.Email.Should().Be(_user.Email);
     }
 }
